73e1c6bd53d59e809b924ea0c7ff0a81b59cee14
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-03-28 22:46:32 +0700

    add example notes and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,79 @@
+import { useState } from "react";
 import "./App.css";
 
+// Create new type and specific the type of each properties
+type Note = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+//Main App
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "Note title 1",
+      description: "Description 1",
+    },
+    {
+      id: 2,
+      title: "Note title 2",
+      description: "Description 1",
+    },
+    {
+      id: 3,
+      title: "Note title 3",
+      description: "Description 1",
+    },
+    {
+      id: 4,
+      title: "Note title 4",
+      description: "Description 1",
+    },
+    {
+      id: 5,
+      title: "Note title 5",
+      description: "Description 1",
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="app-container">
       <form className="note-form">
-        <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required />
+        <h3>Add your note here!</h3>
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Title"
+          required
+        />
+        <textarea placeholder="Description" rows={10} required />
 
-        <button type="submit">Add Note</button>
+        <button className="add-note" type="submit">
+          Add Note
+        </button>
       </form>
       <div className="notes-grid">
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button className="del-note" type="submit">
+                x
+              </button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.description}</p>
+          </div>
+        ))}
         <div className="note-item">
           <div className="notes-header">
-            <button>x</button>
+            <button className="del-note" type="submit">
+              x
+            </button>
           </div>
           <h2>Note Title</h2>
           <p>Note content</p>
