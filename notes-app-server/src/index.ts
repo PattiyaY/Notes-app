e968cf3109010ded9bc1a89f4cad50ab15466de9
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-03-29 23:13:16 +0700

    feature: Insert data and handling error

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -8,9 +8,34 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
+//Retrieve data from a server
 app.get("/api/notes", async (req, res) => {
   const notes = await prisma.note.findMany();
   res.json(notes);
+});
+
+//Sends data to a server to create or update a resource
+app.post("/api/notes", async (req, res) => {
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    return res.status(400).send("title and description fields required");
+  }
+
+  const note = await prisma.note.create({
+    data: { title, description },
+  });
+  res.json(note);
+
+  ///Catch other error
+  try {
+    const note = await prisma.note.create({
+      data: { title, description },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops something went wrong");
+  }
 });
 
 app.listen(5001, () => {
